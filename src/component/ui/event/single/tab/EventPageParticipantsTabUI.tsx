9663e1f311b57f1e520aebf54e2e106de5c2e16f
Alexander Kunakov <kf@kode.ru>
2022-09-17 14:53:25 +0300

    [FT-57] Add participants fetching for event page
    https://buhinder.atlassian.net/browse/FT-57

--- src/component/ui/event/single/tab/EventPageParticipantsTabUI.tsx
+++ src/component/ui/event/single/tab/EventPageParticipantsTabUI.tsx
@@ -1,26 +1,39 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {TabPanel} from "@mui/lab";
 import {Table, TableBody, TableCell, TableContainer, TableRow} from "@mui/material";
+import {useLazyGetUserByIdQuery} from "../../../../../store/api/UserApi";
 
-//todo BUH-25 fetch all by id available
+//todo BUH-25 fetch all by id
 export interface IEventPageParticipantsTabUI {
     participants: string[],
 }
 
-const EventPageParticipantsTabUI: FC<IEventPageParticipantsTabUI> = ({participants}) => (
-    <TabPanel value={"1"}>
-        <TableContainer>
-            <Table>
-                <TableBody>
-                    {participants.map(p => (
-                        <TableRow key={p}>
-                            <TableCell>{p}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    </TabPanel>
-);
+const EventPageParticipantsTabUI: FC<IEventPageParticipantsTabUI> = ({participants}) => {
+    const [users, setUsers] = useState<string[]>([]);
+    const [getUser] = useLazyGetUserByIdQuery();
+
+    useEffect(() => {
+        participants.map(p => getUser(p)
+            .unwrap()
+            .then(res => setUsers(prev => [...prev, `${res.firstname}  ${res.lastName}`]))
+        )
+    }, []);
+
+    return (
+        <TabPanel value={"1"}>
+            <TableContainer>
+                <Table>
+                    <TableBody>
+                        {users.map(u => (
+                            <TableRow key={u}>
+                                <TableCell>{u}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </TabPanel>
+    );
+};
 
 export default EventPageParticipantsTabUI;
